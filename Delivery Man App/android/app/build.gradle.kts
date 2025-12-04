d36491c00bc09f3eb5662e40deeaf347c55c1c46
Monarchgrouptech <137780216+Monarchgrouptech@users.noreply.github.com>
2025-12-04 10:34:05 +0000

    pak

--- Delivery Man App/android/app/build.gradle.kts
+++ Delivery Man App/android/app/build.gradle.kts
@@ -15,7 +15,7 @@ if (keystorePropertiesFile.exists()) {
 }
 
 android {
-    namespace = "com.sixamtech.sixam_mart_delivery_app"
+    namespace = "com.pegasus.vendor"
     compileSdk = 36
     ndkVersion = flutter.ndkVersion
 
@@ -30,7 +30,7 @@ android {
     }
 
     defaultConfig {
-        applicationId = "com.sixamtech.sixam_mart_delivery_app"
+        applicationId = "com.pegasus.vendor"
         minSdk = flutter.minSdkVersion
         targetSdk = 36
         versionCode = flutter.versionCode
